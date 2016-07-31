2f8e9c19b939e86e0a87a2d8fe95ab2b70298d12
Anton Eliasson <devel@antoneliasson.se>
2016-07-31 14:27:53 +0200

    Init with maven-archetype-quickstart

---
+++ src/main/java/se/antoneliasson/kf2monitor/App.java
@@ -0,0 +1,13 @@
+package se.antoneliasson.kf2monitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

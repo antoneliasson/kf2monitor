ca819a2da40626796f2fade3cebf9a8cdd3e11ce
Anton Eliasson <devel@antoneliasson.se>
2016-07-31 19:57:56 +0200

    player: Store Kills instead of Dosh

--- src/main/java/se/antoneliasson/kf2monitor/Player.java
+++ src/main/java/se/antoneliasson/kf2monitor/Player.java
@@ -5,20 +5,21 @@ import java.util.Map;
 public class Player {
     public final String name;
     public final String perk;
-    public final String dosh;
+    public final String kills;
 
     @Override
     public String toString() {
         return "Player{" +
                 "name='" + name + '\'' +
                 ", perk='" + perk + '\'' +
-                ", dosh='" + dosh + '\'' +
+                ", kills='" + kills + '\'' +
                 '}';
     }
 
     public Player(Map<String, String> fields) {
         name = fields.get("Name");
         perk = fields.get("Perk");
-        dosh = fields.get("Dosh");
+        kills = fields.get("Kills");
+
     }
 }

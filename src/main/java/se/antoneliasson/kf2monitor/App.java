b78b0119f82f3525cc4e6ca0bc7e1581e774d39d
Anton Eliasson <devel@antoneliasson.se>
2016-08-01 15:56:09 +0200

    Make WebAdmin host name configurable.

--- src/main/java/se/antoneliasson/kf2monitor/App.java
+++ src/main/java/se/antoneliasson/kf2monitor/App.java
@@ -9,12 +9,12 @@ import static java.lang.System.exit;
 
 public class App 
 {
-    static final String URL = "http://kf2server:8080/ServerAdmin/current/info";
-    static final String USERNAME = "admin";
+    private static final String USERNAME = "admin";
 
-    public App(String username, String password, String host, int port) {
+    public App(String webAdminHost, String username, String password, String host, int port) {
+        String webAdminURL = "http://" + webAdminHost + "/ServerAdmin/current/info";
         BlockingQueue<Message> messages = new LinkedBlockingQueue<>();
-        ProducerThread p = new ProducerThread(messages, URL, username, password);
+        ProducerThread p = new ProducerThread(messages, webAdminHost, username, password);
         Display display = new Display(host, port);
         ConsumerThread c = new ConsumerThread(messages, display);
 
@@ -39,18 +39,19 @@ public class App
 
     public static void main( String[] args )
     {
-        if (args.length < 2) {
-            System.err.println("Usage: java App <webadmin password> <lcdproc host> [lcdproc port]");
+        if (args.length < 3) {
+            System.err.println("Usage: java App <webadmin host[:port]> <webadmin password> <lcdproc host> [lcdproc port]");
             exit(1);
         }
-        String password = args[0];
-        String host = args[1];
+        String webAdminHost = args[0];
+        String password = args[1];
+        String lcdHost = args[2];
         int port;
-        if (args.length > 2) {
-            port = Integer.parseInt(args[2]);
+        if (args.length > 3) {
+            port = Integer.parseInt(args[3]);
         } else {
             port = 13666;
         }
-        new App(USERNAME, password, host, port);
+        new App(webAdminHost, USERNAME, password, lcdHost, port);
     }
 }
